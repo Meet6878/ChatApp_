8940bd613872616d04461fd0058fb53b1eef2ef9
Meet6878 <1160meetsathvara@gmail.com>
2024-02-25 16:31:04 +0530

    change origin --jv

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,27 @@ const app = express();
 
 dotenv.config();
 
-DBconnect();
-
-app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+DBconnect();
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://chatapp-d1jz.onrender.com",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 
 app.use("/api/auth", route);
 app.use("/api/massage", massageroute);
